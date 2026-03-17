f5a5969e7f2c399b5baae730ab01e4db440b58be
Hez <142972013+Zenith-claud@users.noreply.github.com>
2026-03-17 10:16:06 +0700

    add

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 const express = require('express'),
-	cors = require('cors'),
-	secure = require('ssl-express-www');
+const cors = require('cors'),
+const secure = require('ssl-express-www');
 const app = express();
 const bodyParser = require("body-parser");
 const PORT = 3000;
 
-const mainrouter = require("./api/maain.js"),
-	apirouter = require("./api/index.js");
+const main = require("./api/maain.js"),
+	index = require("./api/index.js");
 
 app.use(cors());
 app.enable('trust proxy');
 app.set("json spaces", 2);
-app.use(secure);
-app.use(express.static("public"));
+app.use(secure);app.use(express.static("public"));
 app.set("views", __dirname + "/public");
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/', mainrouter);
-app.use('/api', apirouter);
+app.use(bodyParser.urlencoded({
+ extended: true }));
+app.use('/', main);
+app.use('/api', index);
 
 app.listen(PORT, (error) => {
 	if (!error)
